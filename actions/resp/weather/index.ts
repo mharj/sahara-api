f3bbbf1a115edff0b00d0eec2c52360afa9e16c9
Marko Harjula <marko.harjula@gmail.com>
2018-12-30 10:55:47 +0200

    added weather data keys

--- actions/resp/weather/index.ts
+++ actions/resp/weather/index.ts
@@ -1,7 +1,11 @@
-import {ChannelEnum} from '../../../interfaces';
+import {ChannelEnum, IKeys} from '../../../interfaces';
 import {WeatherResp, WeatherRespAction} from '../../../interfaces/resp/weather';
 import {IApiOpenWeatherMap} from '../../../interfaces/weather';
 
 export const actionWeatherData = (data: IApiOpenWeatherMap): WeatherRespAction => {
 	return {_channel: ChannelEnum.WEATHER, _type: WeatherResp.DATA, data};
+};
+
+export const keysWeatherData = (): IKeys => {
+	return {_channel: ChannelEnum.WEATHER, _type: WeatherResp.DATA};
 };
