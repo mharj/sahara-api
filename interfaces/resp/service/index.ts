f3d3610da4cea5e229b054d0190cb68ad626e37a
Marko Harjula <marko.harjula@gmail.com>
2019-01-15 08:13:57 +0200

    service online/offline

--- interfaces/resp/service/index.ts
+++ interfaces/resp/service/index.ts
@@ -3,10 +3,20 @@ import {IServiceChannel} from '../../serviceChannel';
 
 export enum ServiceResp {
 	LOGGED_IN = 'LOGGED_IN',
+	SET_ONLINE = 'SET_ONLINE',
+	SET_OFFLINE = 'SET_OFFLINE',
 }
 
 export interface IServiceLoggedInRespAction extends IType<ServiceResp>, IServiceChannel {
 	_type: ServiceResp.LOGGED_IN;
+}
+
+export interface IServiceSetOnlineRespAction extends IType<ServiceResp>, IServiceChannel {
+	_type: ServiceResp.SET_ONLINE;
+}
+
+export interface IServiceSetOfflineRespAction extends IType<ServiceResp>, IServiceChannel {
+	_type: ServiceResp.SET_OFFLINE;
 }
 
 export type ServiceRespAction = IServiceLoggedInRespAction;
